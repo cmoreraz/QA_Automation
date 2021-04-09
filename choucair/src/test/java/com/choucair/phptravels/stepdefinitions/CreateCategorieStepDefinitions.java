e92db16816a1f41b416bfdc01454a8592066e09a
cmoreraz <61637059+cmoreraz@users.noreply.github.com>
2021-04-09 13:33:22 -0500

    Update CreateCategorieStepDefinitions.java

--- choucair/src/test/java/com/choucair/phptravels/stepdefinitions/CreateCategorieStepDefinitions.java
+++ choucair/src/test/java/com/choucair/phptravels/stepdefinitions/CreateCategorieStepDefinitions.java
@@ -1,10 +1,14 @@
 package com.choucair.phptravels.stepdefinitions;
 
+import java.util.List;
+
+import com.choucair.phptravels.models.CategorieData;
 import com.choucair.phptravels.tasks.EnterButtonAddCategorie;
 import com.choucair.phptravels.tasks.SelectMenuBlog;
 import com.choucair.phptravels.tasks.SelectSubMenuBlogCat;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.serenitybdd.screenplay.actors.OnStage;
 
@@ -24,5 +28,10 @@ public class CreateCategorieStepDefinitions {
 	@When("^Enter button add categorie$")
 	public void enterButtonAddCategorie() {
 		OnStage.theActorInTheSpotlight().attemptsTo( EnterButtonAddCategorie.enterButtonAddCategorie() );
+	}
+	
+	@Then("^Enter the form data and add the category$")
+	public void enterTheFormDataAndAddTheCategory( List<CategorieData> dataCategorie ) {
+
 	}
 }
